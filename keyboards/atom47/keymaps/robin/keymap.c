1449a6ae4e93fdf2af5a5f604102b70b4e824146
gilmoregrills <robinfarrowyonge@gmail.com>
2018-12-29 14:30:53 +0000

    add emojis, some macros

---
+++ keyboards/atom47/keymaps/robin/keymap.c
@@ -0,0 +1,101 @@
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+
+#define _MA 0 //Main layer
+#define _FN 1 //Fn
+#define _FN1 3 //Fn1
+#define _FN2 4 //Fn2
+
+#define _______ KC_TRNS
+
+// void matrix_init_user() {
+//   set_unicode_input_mode(UNICODE_MODE_OSX);
+// }
+
+enum custom_keycodes {
+  M_EMAIL = SAFE_RANGE,
+  M_EMAIL2,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case M_EMAIL:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING("robinfarrowyonge@gmail.com");
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case M_EMAIL2:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING("robin.farrow-yonge@news.co.uk");
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+
+  }
+  return true;
+};
+
+enum unicode_aliases {
+  E_QQ,
+  E_WINK,
+  E_EGGP,
+  E_RAIN,
+  E_TONG,
+  E_YY,
+  E_SNEK,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+  [E_QQ]   = 0x1F62D, // 😭
+  [E_WINK] = 0x1F609, // 😉
+  [E_EGGP] = 0x1F346, // 🍆
+  [E_RAIN] = 0x1F4A6, // 💦
+  [E_TONG] = 0x1F61B, // 😛
+  [E_YY]   = 0x1F914, // 🤔
+  [E_SNEK] = 0x1F40D, // 🐍
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_MA] = LAYOUT(
+  KC_ESC,       KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,   \
+  KC_TAB,       KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,                KC_ENT,    \
+  KC_LSPO,                  KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_RSPC,    MO(_FN),   \
+  KC_LCTL,      KC_LALT,    KC_LGUI,    MO(_FN),                LT(_FN1, KC_SPC),       KC_BSPC,                MO(_FN2),   KC_MINUS,   KC_APP,     KC_RCTRL), \
+
+[_FN] = LAYOUT(
+  KC_GRV,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_LPRN,    KC_RPRN,    \
+  _______,      KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_EQL,     KC_QUOT,                _______,    \
+  _______,                  KC_BSLS,    KC_GRAVE,   _______,    _______,    _______,    _______,    _______,    _______,    KC_SLSH,    _______,    _______,    \
+  _______,      _______,    _______,    _______,                _______,                KC_DEL,                 _______,    _______,    _______,    _______),   \
+
+[_FN1] = LAYOUT(
+  KC_POWER,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     \
+  _______,      _______,    M_EMAIL,    M_EMAIL2,   _______,    _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    _______,                _______,    \
+  _______,                  _______,    _______,    _______,    _______,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,    _______,    _______,    \
+  _______,      _______,    _______,    _______,                _______,                KC_DEL,                 _______,    _______,    _______,    RESET),     \
+
+[_FN2] = LAYOUT(
+  UC_M_OS,     X(E_QQ),     X(E_WINK),  X(E_EGGP),  X(E_RAIN),  X(E_TONG),  X(E_YY),    _______,    _______,    _______,    _______,    _______,    _______,    \
+  _______,     _______,     X(E_SNEK),  _______,    _______,    _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    _______,                _______,    \
+  _______,                  _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    \
+  _______,     _______,     _______,    _______,                _______,                _______,                _______,    _______,    _______,    _______),   \
+};
+
+void led_set_user(uint8_t usb_led) {
+    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+        // Turn capslock on
+        PORTE &= ~(1 << 6);
+    } else {
+        // Turn capslock offp
+        PORTE |= (1 << 6);
+    }
+}
--- keyboards/atom47/keymaps/robin/rules.mk
+++ keyboards/atom47/keymaps/robin/rules.mk
@@ -51,17 +51,13 @@ BOOTLOADER= qmk-dfu
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no	# Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
-CONSOLE_ENABLE = no	# Console for debug(+400)
+BOOTMAGIC_ENABLE = no   # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = no    # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes   # Audio control and System control(+450)
+CONSOLE_ENABLE = no # Console for debug(+400)
 COMMAND_ENABLE = no    # Commands for debug and configuration
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality (+1150)
-MIDI_ENABLE = no 		# MIDI controls
+MIDI_ENABLE = no        # MIDI controls
 AUDIO_ENABLE = no
-UNICODE_ENABLE = no 		# Unicode
+UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no # Enable Bluetooth with the Adafruit EZ-Key HID
-UNICODEMAP_ENABLE = yes
-
-DEFAULT_FOLDER = atom47/rev3
-
